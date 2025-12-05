13aa214554ddb0270a6cbcf32c83a17c729d19cb
giit-hutabarat <fernando.hutabarat25@gmail.com>
2025-12-05 12:56:16 +0700

    koreksi modul

--- app/Modules/Setting/Config/Routes.php
+++ app/Modules/Setting/Config/Routes.php
@@ -6,16 +6,7 @@ if(!isset($routes))
 }
 
 // ====================================================================
-// KELOMPOK 1: WEB ROUTE (HALAMAN ADMIN)
-// ====================================================================
-// Gunakan use ($adminSetupController) agar variabel dapat diakses di dalam closure.
-$routes->group('setting', ['filter' => 'auth_session', 'namespace' => 'App\\Modules\\Setting\\Controllers'], function($routes) use ($adminSetupController){ 
-	$routes->add('general', 'Setting::general');
-	$routes->add('app', 'Setting::app');
-});
-
-// ====================================================================
-// KELOMPOK 2: ADMIN API ROUTE (CRUD & Update Config)
+// KELOMPOK ADMIN API ROUTE (CRUD & Update Config)
 // ... (API routes lainnya dari Modul Setting) ...
 $routes->group('api', ['filter' => 'auth_session', 'namespace' => 'App\\Modules\\Setting\\Controllers\\Api'], function($routes){
     $routes->get('setting/general', 'ApiSetting::general');
